e8b98d72c6c6f1a6097c967b0e9dd9c5e815e6a7
shsong <shsong@adenasoft.com>
2024-08-29 22:45:37 +0900

    Feat: Practice on cryptography and the solana network

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,18 @@
+// 1. Keypair 생성
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`✅ Finished!`);
+
+
+// 2. 로컬 SecretKey 불러오기
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+ 
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+ 
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
